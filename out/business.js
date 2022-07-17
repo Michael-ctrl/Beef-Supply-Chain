6f2266cf1dfc702fcc5fe1d50c14a3cb1c035719
Cooper van Wijck <coopervanw@gmail.com>
2022-07-17 14:40:33 +1000

    UI Setup

---
+++ out/business.js
@@ -0,0 +1,11 @@
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('COol');
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=business.js.map
---
+++ ui/business/business.ts
@@ -0,0 +1,7 @@
+class Startup {
+    public static main(): number {
+        console.log('Hello World!');
+        return 0;
+        }
+    }
+    Startup.main();
