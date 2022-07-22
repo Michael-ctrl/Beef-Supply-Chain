09b5832883e919bcfd47acc800ebbe2d03f1943b
Michael <michael.liang@outlook.com.au>
2022-07-22 16:36:56 +1000

    Added MeatInfo struct and tested it to be working

--- smartcontracts/MeatNFT.sol
+++ smartcontracts/MeatNFT.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MeatNFT is ERC721 {
+contract MeatNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -17,13 +18,13 @@ contract MeatNFT is ERC721 {
         bool voted;
     }
 
+/*
     struct Node {
         bytes32 name;     // name of node
         bytes32 parent;   // parent node’s path
         bytes32 data;     // node’s data
         bytes32[] nodes;  // list of linked nodes’ paths
     }
-/*
     mapping(bytes32 => Node) nodes;
 function get(bytes32 _name, bytes32 _parent) public view returns (bytes32, bytes32, bytes32, bytes32[]) {
     Node storage node = nodes[keccak256(_parent, _name)];
@@ -70,27 +71,34 @@ function getIndex(bytes32[] childs, bytes32 _path) pure internal returns (uint25
     return childs.length - 1;
 }*/
 
-    struct History {}
+    // struct History {}
 
 
     mapping (uint256 => MeatInfo) public idToInfo;
-    mapping (uint256 => History) public idToHistory;
+    // mapping (uint256 => History) public idToHistory;
 
-    constructor() public ERC721("MeatNFT", "BFT") { }
+    constructor() public ERC721("MeatNFT", "BFT") {}
 
-    function (address player, string memory tokenURI)
-        public
-        returns (uint256)
-    {
-        _tokenIds.increment();
-
+    function createMeat(string memory tokenURI, string memory _description, string memory _location, uint _dateCreated, uint _weight, uint _grade) public returns (uint256) {
         uint256 newItemId = _tokenIds.current();
-        _mint(player, newItemId);
+        _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
+
+        MeatInfo memory m;
+        m.description = _description;
+        m.location = _location;
+        m.dateCreated = _dateCreated;
+        m.weight = _weight;
+        m.grade = _grade;
+        m.voted = false;
+        idToInfo[_tokenIds.current()] = m;
+
+        _tokenIds.increment();
 
         return newItemId;
     }
 
+    /*
     // Check if tokenID is owned by msg.sender and then append to the voting list
     function intiateVoting(uint256 tokenId) {
         ownerOf()
@@ -111,5 +119,6 @@ function getIndex(bytes32[] childs, bytes32 _path) pure internal returns (uint25
     function mintMeatToken (lots of data) {
 
     }
+    */
 
 }
