142fb1e703c16b7b407fd5148c0f02ea2a8b4142
Marcus <85930095+HsuMuKuan@users.noreply.github.com>
2022-07-22 21:23:20 +1000

    Update Vote.sol
    Need to add corresponding meat update function in meatnft

--- smartcontracts/Vote.sol
+++ smartcontracts/Vote.sol
@@ -14,13 +14,21 @@ contract Voting{
     uint public numVotes = 0;
     uint grade_total = 0;
     address public manager;                     //Manager
+    MeatNFT meatnft_contract;
+    //mapping for queue
+    mapping(uint256 => uint256) public queue;
+    uint256 queue_first = 1;
+    uint256 queue_last = 0;
 
-    mapping (address => Vote) private votes;       //List of vote grade 
+    //List of vote grade 
+    mapping (address => Vote) private votes;      
+
+    // starting state; 
     voteState public state = voteState.create; 
     uint256 startingBlock = 0; 
     uint public result_grade = 0;
-    // block limit used for timeout
-    uint blocklimit  = 100;
+    uint256 public current_meat = 0;
+    uint blocklimit  = 100;// block limit used for timeout
 
     // Creates a new lunch venue contract
     constructor () {
@@ -28,11 +36,35 @@ contract Voting{
         startingBlock = block.number;
     }
 
+    // update the external function address
+    function get_meatnft(address addr) public {
+        meatnft_contract = MeatNFT(addr);
+    } 
+
+    //  function for meat grade enqueue
+    function meat_enqueue(uint256 meat) public{
+        queue_last += 1;
+        queue[queue_last] = meat;
+        if(state == voteState.create)
+            startVoting();
+    }
+
+    // function for meat grade dequeue
+    function dequeue() public returns (uint256 meat){
+        require(queue_last >= queue_first); // check if queue is empty
+
+        meat = queue[queue_first];
+        delete queue[queue_first];
+        queue_first += 1;
+    }
+
 
     // @notice manager force contract to enter voting phase
     // @return true if currectly transfered to voting phase
     function startVoting() public restricted votingCreate returns (bool){
+        require(queue_last >= queue_first); // check if meat queue is empty 
         if(state == voteState.create){
+            current_meat = queue[queue_first];
             state = voteState.open;
             return true;
         } else{
@@ -41,7 +73,8 @@ contract Voting{
     }
 
     /// @return validVote Is the vote valid? or trigger final result if 10 regulator has voted or block limit exceed
-    function doVote(uint grade, uint256 validation_key) public votingOpen returns (bool validVote){
+    function doVote(uint grade, uint256 validation_key, uint256 meat) public votingOpen returns (bool validVote){
+        require(queue[queue_first] == meat); // check is meat id matches currently voted one
         validVote = false;                              //Is the vote valid?
         if(block.number > startingBlock + blocklimit){
             finalResult();
@@ -52,7 +85,7 @@ contract Voting{
             if(votes[msg.sender].grade == 0){ // check if the sender has voted before
                 numVotes = numVotes+1;
                 grade_total = grade + grade;
-            }else{
+            }else{ // update the vote if the voter has voted before
                 grade_total = grade_total + grade - votes[msg.sender].grade;
             }
             votes[msg.sender].grade = grade;
@@ -69,7 +102,12 @@ contract Voting{
     /// @dev If top 2 venues have the same no of votes, final result depends on vote order
     function finalResult() private{ 
         result_grade = grade_total / numVotes;
-        state = voteState.end; //Voting is now closed
+        meatnft_contract.set_meat_grade(result_grade, current_meat);
+        dequeue();
+        state = voteState.create; //Voting state transfered to create
+        if(queue_last >= queue_first){ // change the state to 
+            startVoting();
+        } 
     }
 
     ///@notice function force all functions to end
@@ -100,4 +138,8 @@ contract Voting{
     } 
 
 
+}
+
+contract MeatNFT{
+    function set_meat_grade(uint256 grade, uint256 toeknid) public {}
 }
